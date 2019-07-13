bb3f45611ceb08b5d7db5a1a01e9276bdb17b2a7
Tokket Dev <tokketdev@gmail.com>
2019-07-13 06:04:46 -0700

    multiplayerapiversion0.1 group api working in asp.net core

---
+++ MultiplayerClient/MultiplayerClient/Hubs/ChatHub.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MultiplayerClient.Hubs
+{  
+    public class ChatHub : Hub
+    {
+
+
+      
+        private const string codePrefix = "?code=";
+        private const string apiKey = "3zAAVfyq47XbeMEvUmn855SXjTMZf/Z7nt0Cj5UrQ3w/UCCuHheGjQ==";
+        static HttpClient client = new HttpClient();
+
+
+
+        public async Task GetSignalRConnectionInfo()
+        {
+            string baseUrl = "http://localhost:7071/api/v1/negotiate";
+            var result = await client.PostAsync($"{baseUrl}", null);
+
+            var res = JsonConvert.DeserializeObject<SignalRConnectionInfo>(await result.Content.ReadAsStringAsync());
+
+            await Clients.Caller.SendAsync("ReceiveMessage2", res);
+
+       
+        }
+
+        public async Task addtogroup(string id, Group item)
+        {
+            string baseUrl = "http://localhost:7071/api/v1/addgroup/";
+
+            var result = await client.PostAsJsonAsync($"{baseUrl}{id}", item);
+
+            await Clients.Caller.SendAsync("ReceiveFromAdd", result);
+            var res = await result.Content.ReadAsStringAsync();
+
+            await Clients.Caller.SendAsync("ReceiveMessage3", res);
+
+            //await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            //await Clients.Group(groupName).SendAsync("ReceiveMessage3", $"{Context.ConnectionId} has joined the group {groupName}.");
+
+        }
+
+        //int count = 0;
+        //public async Task AddToGroup(string groupName)
+        //{
+        //    count++;
+        //    await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+        //    await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has {count}joined the group {groupName}.");
+        //}
+
+        //public async Task RemoveFromGroup(string groupName)
+        //{
+        //    await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+        //    await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        //}
+
+
+        //public async Task messageSend(string groupName, string message)
+        //{
+        //   // string name = Context.ConnectionId;
+
+        //    await Clients.Group(groupName).SendAsync("Send", message);
+
+        //}
+
+
+
+
+
+        public async Task sendThis(string message, Group item)
+        {
+            string baseUrl = " http://localhost:7071/api/v1/SendGroupMessage/";
+
+            var result = await client.PostAsJsonAsync($"{baseUrl}{message}",item);
+
+            var res = await result.Content.ReadAsStringAsync();
+            await Clients.Caller.SendAsync("newMessageagain", res);
+
+
+            //     var res = JsonConvert.DeserializeObject<string>(await result.Content.ReadAsStringAsync());
+
+            //   await Clients.Caller.SendAsync("ReceiveMessage3", res);
+
+            //await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            //await Clients.Group(groupName).SendAsync("ReceiveMessage3", $"{Context.ConnectionId} has joined the group {groupName}.");
+
+        }
+
+
+
+
+        //public async Task MultiplayerSetup2(GamePlayer player)
+        //{
+        //    string newUrl = "";
+        //    var result = await client.PostAsync($"{newUrl}/multiplayersetup{codePrefix}{apiKey}", new StringContent(JsonConvert.SerializeObject(player)));
+        //    var res = JsonConvert.DeserializeObject<SignalRConnectionInfo>(await result.Content.ReadAsStringAsync());
+
+        //    await Clients.Caller.SendAsync("ReceiveMessage3", res);
+
+
+        //}
+
+
+
+
+
+
+        //public async Task SendMessage(string user, string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+    }
+}
+
+
+public class Group
+{
+    [JsonProperty("group_name")]
+    public string Group_name { get; set; }
+
+}
+
+public class SignalRConnectionInfo
+{
+    [JsonProperty("url")]
+    public string Url { get; set; }
+    [JsonProperty("accessToken")]
+    public string AccessToken { get; set; }
+}
+
+
+public class GamePlayer
+{
+    [JsonProperty("user_id")]
+    public string UserId { get; set; }
+
+    [JsonProperty("user_name")]
+    public string UserName { get; set; }
+
+    [JsonProperty("user_photo")]
+    public string UserPhoto { get; set; }
+
+    [JsonProperty("player_number", NullValueHandling = NullValueHandling.Ignore)]
+    public int? PlayerNumber { get; set; } = null;
+}
+public class GameInfo
+{
+    [JsonProperty("id")]
+    public string Id { get; set; } = "gameinfo-" + Guid.NewGuid().ToString();
+
+    [JsonProperty("players")]
+    public List<GamePlayer> Players { get; set; } = new List<GamePlayer>();
+
+    [JsonProperty("room_id")]
+    public string RoomId { get; set; }
+
+    [JsonProperty("toks")]
+    public List<int> Toks { get; set; }
+
+    [JsonProperty("content")]
+    public dynamic Content { get; set; }
+}
+
+
+public class TokBlitzRace : GameInfo
+{
+    [JsonProperty("player1_points")]
+    public int Player1Points { get; set; }
+
+    [JsonProperty("player2_points")]
+    public int Player2Points { get; set; }
+
+    [JsonProperty("player1_points_per_round")]
+    public List<int> Player1PointsPerRound { get; set; } //Example for rounds (1-5): [100,50,75,25,100]. Do not count Finished first points, that is added in the api
+
+    [JsonProperty("player2_points_per_round")]
+    public List<int> Player2PointsPerRound { get; set; }
+
+    [JsonProperty("player1_guess")]
+    public int Player1Guess { get; set; }
+
+    [JsonProperty("player2_guess")]
+    public int Player2Guess { get; set; }
+
+    [JsonProperty("player1_round")]
+    public int Player1Round { get; set; }
+
+    [JsonProperty("player2_round")]
+    public int Player2Round { get; set; }
+
+    [JsonProperty("seconds_elapsed")]
+    public int SecondsElasped { get; set; }
+
+    [JsonProperty("finished_first")]
+    public GamePlayer FinishedFirst { get; set; }
+
+    [JsonProperty("winner")]
+    public GamePlayer Winner { get; set; }
+}
+
---
+++ MultiplayerClient/MultiplayerClient/wwwroot/js/Chat.js
@@ -0,0 +1,197 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.start().then(function (ans) {
+    console.log(ans);
+
+}).catch(function (err) {
+        return console.error(err.toString());
+
+
+ });
+
+//    document.getElementById("sendButton").addEventListener("click", function (event) {
+//    var getter = document.getElementById('userInput').value;
+
+//        connection.invoke("AddToGroup", getter).catch(function (err) {
+//            return console.error(err.toString());
+
+//        });
+       
+//});
+
+
+document.getElementById("removeButton").addEventListener("click", function (event) {
+    var getter2 = document.getElementById('userInput').value;
+    
+    connection.invoke("RemoveFromGroup", getter2).catch(function (err) {
+        return console.error(err.toString());
+
+    });
+
+});
+
+document.getElementById("sendrealmessage").addEventListener("click", function (event) {
+    var getter2 = document.getElementById('userInput').value;
+    var getter3 = document.getElementById('messageInput').value;
+
+    connection.invoke("messageSend", getter2, getter3).catch(function (err) {
+        return console.error(err.toString());
+
+    });
+
+});
+
+
+
+connection.on("SendR", function (ans5) {
+
+    console.log(ans5);
+
+});
+
+
+
+        connection.on("Send", function (ans5) {
+
+            console.log(ans5);
+
+        });
+
+
+
+
+
+
+
+
+connection.on("newMessage", function (ans5) {
+
+    console.log(ans5);
+
+});
+   
+
+
+document.getElementById("multiConnect").addEventListener("click", function (event) {
+
+    connection.invoke("GetSignalRConnectionInfo").catch(function (err) {
+        
+        return console.error(err.toString());
+
+    });
+    event.preventDefault();
+
+});
+
+document.getElementById("sender").addEventListener("click", function (event) {
+    // userId
+    var getter2 = document.getElementById('userInput').value;
+
+    // groupname
+    var setter = document.getElementById('messageInput').value;
+    var getter3 = {
+        group_name: setter
+    }; 
+
+    connection.invoke("sendThis", getter2, getter3).catch(function (err) {
+        return console.error(err.toString());
+
+    });
+    event.preventDefault();
+         
+
+});  
+
+
+
+
+
+
+document.getElementById("addtogroup").addEventListener("click", function (event) {
+    //userId
+    var getter2 = document.getElementById('userInput').value;
+
+    // groupname
+    var setter = document.getElementById('messageInput').value;
+    var getter3 = {
+        group_name: setter
+    }; 
+
+    connection.invoke("addtogroup", getter2, getter3).catch(function (err) {
+        return console.error(err.toString());
+
+    });
+    event.preventDefault();
+
+});
+
+
+connection.on("ReceiveFromAdd", function (ans5) {
+
+    console.log(ans5);
+
+});
+
+
+
+
+connection.on("ReceiveMessage3", function (ans5) {
+
+    console.log(ans5);
+
+});
+
+
+
+connection.on("newMessageagain", function (ans5) {
+
+    console.log(ans5);
+
+});
+
+
+var connection2;
+connection.on("ReceiveMessage2", function (ans) {
+            
+    connection2 = new signalR.HubConnectionBuilder()
+
+        .withUrl(ans.url, { accessTokenFactory: () => ans.accessToken })
+        .build();
+
+
+        connection2.start().then(function () {
+
+
+            console.log("connected");
+
+            connection2.on("newMessage", function (ans5) {
+
+                console.log(ans5);
+
+            });
+
+            connection2.on("ReceiveMessage3", function (ans5) {
+
+                console.log(ans5);
+
+            });
+
+            connection2.on("ReceiveFromAdd", function (ans5) {
+
+                console.log(ans5);
+
+            });
+
+
+
+
+
+
+        }).catch(function (err) {
+            return console.error(err.toString());
+
+        });
+
+      
+         
+});
